4b77386fc31cb04e1109706f71bbe8d782612851
Tom <tom.clement@bizboard.nl>
2019-07-02 23:32:25 +0200

    Cleaned up hub75 driver init

--- firmware/components/driver_display_hub75/driver_hub75.c
+++ firmware/components/driver_display_hub75/driver_hub75.c
@@ -1,17 +1,22 @@
 #include <esp_err.h>
+#include <sdkconfig.h>
+
+#ifdef CONFIG_DRIVER_HUB75_ENABLE
 
 #include "include/driver_hub75.h"
 #include "include/displayDriver.h"
 
 
 /**
- * Will be called from /firmware/main/main.c during initialisation (be sure to #include <driver_hub75.h> there).
+ * Will be called from /firmware/main/main.c during initialisation using the INIT_DRIVER macro.
  */
 esp_err_t driver_hub75_init(void) {
-
-#ifdef CONFIG_DRIVER_I2C_ENABLE
     displayDriver_init();
-#endif
 
     return ESP_OK;
 }
+
+
+#else // DRIVER_HUB75_ENABLE
+esp_err_t driver_hub75_init(void) { return ESP_OK; } // Dummy function, leave empty!
+#endif
