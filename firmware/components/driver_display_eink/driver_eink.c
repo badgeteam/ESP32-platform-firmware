eab7a21d478c4f7e5512535cb1e85b190ca0c383
Renze Nicolai <renze@rnplus.nl>
2019-07-05 22:05:12 +0200

    Add files

---
+++ firmware/components/driver_display_eink/driver_eink.c
@@ -0,0 +1,464 @@
+#include <sdkconfig.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <esp_heap_caps.h>
+#include <esp_log.h>
+
+#include "driver_pins.h"
+#include "driver_eink_dev.h"
+#include "driver_eink_lut.h"
+#include "driver_eink.h"
+
+#ifdef CONFIG_DRIVER_EINK_ENABLE
+
+static const char *TAG = "e-ink";
+
+static const uint8_t xlat_curve[256] = {
+    0x00,0x01,0x01,0x02,0x02,0x03,0x03,0x03,0x04,0x04,0x05,0x05,
+    0x06,0x06,0x07,0x07,0x08,0x08,0x09,0x09,0x0a,0x0a,0x0a,0x0b,
+    0x0b,0x0c,0x0c,0x0d,0x0d,0x0e,0x0e,0x0f,0x0f,0x10,0x10,0x11,
+    0x11,0x12,0x12,0x13,0x13,0x14,0x15,0x15,0x16,0x16,0x17,0x17,
+    0x18,0x18,0x19,0x19,0x1a,0x1a,0x1b,0x1b,0x1c,0x1d,0x1d,0x1e,
+    0x1e,0x1f,0x1f,0x20,0x20,0x21,0x22,0x22,0x23,0x23,0x24,0x25,
+    0x25,0x26,0x26,0x27,0x27,0x28,0x29,0x29,0x2a,0x2a,0x2b,0x2c,
+    0x2c,0x2d,0x2e,0x2e,0x2f,0x2f,0x30,0x31,0x31,0x32,0x33,0x33,
+    0x34,0x35,0x35,0x36,0x37,0x37,0x38,0x39,0x39,0x3a,0x3b,0x3b,
+    0x3c,0x3d,0x3e,0x3e,0x3f,0x40,0x40,0x41,0x42,0x43,0x43,0x44,
+    0x45,0x46,0x46,0x47,0x48,0x49,0x49,0x4a,0x4b,0x4c,0x4c,0x4d,
+    0x4e,0x4f,0x50,0x50,0x51,0x52,0x53,0x54,0x55,0x55,0x56,0x57,
+    0x58,0x59,0x5a,0x5b,0x5b,0x5c,0x5d,0x5e,0x5f,0x60,0x61,0x62,
+    0x63,0x64,0x65,0x66,0x66,0x67,0x68,0x69,0x6a,0x6b,0x6c,0x6d,
+    0x6e,0x6f,0x70,0x71,0x72,0x74,0x75,0x76,0x77,0x78,0x79,0x7a,
+    0x7b,0x7c,0x7d,0x7e,0x80,0x81,0x82,0x83,0x84,0x86,0x87,0x88,
+    0x89,0x8a,0x8c,0x8d,0x8e,0x90,0x91,0x92,0x93,0x95,0x96,0x98,
+    0x99,0x9a,0x9c,0x9d,0x9f,0xa0,0xa2,0xa3,0xa5,0xa6,0xa8,0xa9,
+    0xab,0xac,0xae,0xb0,0xb1,0xb3,0xb5,0xb6,0xb8,0xba,0xbc,0xbe,
+    0xbf,0xc1,0xc3,0xc5,0xc7,0xc9,0xcb,0xcd,0xcf,0xd1,0xd3,0xd6,
+    0xd8,0xda,0xdc,0xdf,0xe1,0xe3,0xe6,0xe8,0xeb,0xed,0xf0,0xf3,
+    0xf5,0xf8,0xfb,0xfe,
+};
+
+static uint32_t *driver_eink_tmpbuf = NULL;
+static uint32_t *driver_eink_oldbuf = NULL;
+static bool driver_eink_have_oldbuf = false;
+
+static void
+memcpy_u32(uint32_t *dst, const uint32_t *src, size_t size)
+{
+	while (size-- > 0)
+	{
+		*dst++ = *src++;
+	}
+}
+
+static void
+memset_u32(uint32_t *dst, uint32_t value, size_t size)
+{
+	while (size-- > 0)
+	{
+		*dst++ = value;
+	}
+}
+
+static void
+driver_eink_create_bitplane(const uint8_t *img, uint32_t *buf, int bit, driver_eink_flags_t flags)
+{
+#ifdef EPD_ROTATED_180
+	flags ^= DISPLAY_FLAG_ROTATE_180;
+#endif
+	int x, y;
+	int pos, dx, dy;
+	if (flags & DISPLAY_FLAG_ROTATE_180)
+	{
+		pos = DISP_SIZE_Y-1;
+		dx = DISP_SIZE_Y;
+		dy = -DISP_SIZE_Y*DISP_SIZE_X - 1;
+	}
+	else
+	{
+		pos = (DISP_SIZE_X-1)*DISP_SIZE_Y;
+		dx = -DISP_SIZE_Y;
+		dy = DISP_SIZE_Y*DISP_SIZE_X + 1;
+	}
+	for (y = 0; y < DISP_SIZE_Y; y++) {
+		for (x = 0; x < DISP_SIZE_X;) {
+			int x_bits;
+			uint32_t res = 0;
+			for (x_bits=0; x_bits<32; x_bits++)
+			{
+				res <<= 1;
+				if (flags & DISPLAY_FLAG_8BITPIXEL)
+				{
+					uint8_t pixel = img[pos];
+					pos += dx;
+					int j = xlat_curve[pixel];
+					if ((j & bit) != 0)
+						res++;
+				}
+				else
+				{
+					uint8_t pixel = img[pos >> 3] >> (pos & 7);
+					pos += dx;
+					if ((pixel & 1) != 0)
+						res++;
+				}
+				x++;
+			}
+			*buf++ = res;
+		}
+		pos += dy;
+	}
+}
+
+static void
+driver_eink_set_ram_area(uint8_t x_start, uint8_t x_end,
+		uint16_t y_start, uint16_t y_end)
+{
+	// set RAM X - address Start / End position
+	driver_eink_dev_write_command_p2(0x44, x_start, x_end);
+	// set RAM Y - address Start / End position
+	driver_eink_dev_write_command_p4(0x45, y_start & 0xff, y_start >> 8, y_end & 0xff, y_end >> 8);
+}
+
+static void
+driver_eink_set_ram_pointer(uint8_t x_addr, uint16_t y_addr)
+{
+	// set RAM X address counter
+	driver_eink_dev_write_command_p1(0x4e, x_addr);
+	// set RAM Y address counter
+	driver_eink_dev_write_command_p2(0x4f, y_addr & 0xff, y_addr >> 8);
+}
+
+static void
+driver_eink_write_bitplane(const uint32_t *buf)
+{
+	driver_eink_set_ram_area(0, DISP_SIZE_X_B - 1, 0, DISP_SIZE_Y - 1);
+	driver_eink_set_ram_pointer(0, 0);
+	driver_eink_dev_write_command_stream_u32(0x24, buf, DISP_SIZE_X_B * DISP_SIZE_Y/4);
+}
+
+void
+driver_eink_update(const uint32_t *buf, const struct driver_eink_update *upd_conf)
+{
+	// generate lut data
+	const struct driver_eink_lut_entry *lut_entries;
+
+	if (upd_conf->lut == DRIVER_EINK_LUT_CUSTOM)
+	{
+		lut_entries = upd_conf->lut_custom;
+	}
+	else if (upd_conf->lut >= 0 && upd_conf->lut <= DRIVER_EINK_LUT_MAX)
+	{
+		const struct driver_eink_lut_entry *lut_lookup[DRIVER_EINK_LUT_MAX + 1] = {
+			driver_eink_lut_full,
+			driver_eink_lut_normal,
+			driver_eink_lut_faster,
+			driver_eink_lut_fastest,
+		};
+		lut_entries = lut_lookup[upd_conf->lut];
+	}
+	else
+	{
+		lut_entries = driver_eink_lut_full;
+	}
+
+	uint8_t lut[DRIVER_EINK_LUT_MAX_SIZE];
+	int lut_len = driver_eink_lut_generate(lut_entries, upd_conf->lut_flags, lut);
+	assert( lut_len >= 0 );
+
+	driver_eink_dev_write_command_stream(0x32, lut, lut_len);
+
+	if (buf == NULL)
+		buf = driver_eink_tmpbuf;
+
+	driver_eink_write_bitplane(buf);
+
+	if (driver_eink_dev_type == DRIVER_EINK_DEPG0290B1 && driver_eink_have_oldbuf)
+		driver_eink_dev_write_command_stream_u32(0x26, driver_eink_oldbuf, DISP_SIZE_X_B * DISP_SIZE_Y/4);
+
+	// write number of overscan lines
+	driver_eink_dev_write_command_p1(0x3a, upd_conf->reg_0x3a);
+
+	// write time to write every line
+	driver_eink_dev_write_command_p1(0x3b, upd_conf->reg_0x3b);
+
+	uint16_t y_len = upd_conf->y_end - upd_conf->y_start;
+	// configure length of update
+	driver_eink_dev_write_command_p3(0x01, y_len & 0xff, y_len >> 8, 0x00);
+
+	// configure starting-line of update
+	driver_eink_dev_write_command_p2(0x0f, upd_conf->y_start & 0xff, upd_conf->y_start >> 8);
+
+	// bitmapped enabled phases of the update: (in this order)
+	//   80 - enable clock signal
+	//   40 - enable CP
+	//   20 - load temperature value
+	//   10 - load LUT
+	//   08 - initial display
+	//   04 - pattern display
+	//   02 - disable CP
+	//   01 - disable clock signal
+	driver_eink_dev_write_command_p1(0x22, 0xc7);
+
+	// start update
+	driver_eink_dev_write_command(0x20);
+
+	if (driver_eink_dev_type == DRIVER_EINK_DEPG0290B1)
+	{
+		memcpy_u32(driver_eink_oldbuf, buf, DISP_SIZE_X_B * DISP_SIZE_Y/4);
+	}
+	driver_eink_have_oldbuf = true;
+}
+
+void
+driver_eink_display(const uint8_t *img, driver_eink_flags_t flags)
+{
+	int lut_mode = 
+		(flags >> DISPLAY_FLAG_LUT_BIT) & ((1 << DISPLAY_FLAG_LUT_SIZE)-1);
+
+	uint32_t *buf = driver_eink_tmpbuf;
+	if (img == NULL)
+	{
+		memset_u32(buf, 0, DISP_SIZE_X_B * DISP_SIZE_Y/4);
+	}
+	else
+	{
+		driver_eink_create_bitplane(img, buf, 0x80, flags);
+	}
+
+	if ((flags & DISPLAY_FLAG_NO_UPDATE) != 0)
+	{
+		return;
+	}
+
+	int lut_flags = 0;
+	if (!driver_eink_have_oldbuf || (flags & DISPLAY_FLAG_FULL_UPDATE))
+	{
+		// old image not known (or full update requested); do full update
+		lut_flags |= LUT_FLAG_FIRST;
+	}
+	else if (lut_mode - 1 != DRIVER_EINK_LUT_FULL)
+	{
+		// old image is known; prefer to do a partial update
+		lut_flags |= LUT_FLAG_PARTIAL;
+	}
+
+	struct driver_eink_update eink_upd = {
+		.lut       = lut_mode > 0 ? lut_mode - 1 : DRIVER_EINK_LUT_DEFAULT,
+		.lut_flags = lut_flags,
+		.reg_0x3a  = 26,   // 26 dummy lines per gate
+		.reg_0x3b  = 0x08, // 62us per line
+		.y_start   = 0,
+		.y_end     = 295,
+	};
+	driver_eink_update(buf, &eink_upd);
+}
+
+void
+driver_eink_display_greyscale(const uint8_t *img, driver_eink_flags_t flags, int layers)
+{
+	// start with black.
+	driver_eink_display(NULL, flags | DISPLAY_FLAG_FULL_UPDATE);
+
+	// the max. number of layers. more layers will result in more ghosting
+	if (driver_eink_dev_type == DRIVER_EINK_DEPG0290B1 && layers > 5) {
+		layers = 5;
+	} else if (driver_eink_dev_type == DRIVER_EINK_GDEH029A1 && layers > 7) {
+		layers = 7;
+	}
+
+	int p_ini = (driver_eink_dev_type == DRIVER_EINK_DEPG0290B1) ? 4 : 16;
+
+	driver_eink_have_oldbuf = false;
+
+	int layer;
+	for (layer = 0; layer < layers; layer++) {
+		int bit = 128 >> layer;
+		int t = bit;
+		// gdeh: 128, 64, 32, 16, 8, 4, 2
+		// depg: 128, 64, 32, 16, 8
+
+		int p = p_ini;
+
+		while ((t & 1) == 0 && (p > 1)) {
+			t >>= 1;
+			p >>= 1;
+		}
+
+		if (driver_eink_dev_type == DRIVER_EINK_DEPG0290B1 && driver_eink_have_oldbuf == false && p == 1 && t > 1 && layer+1 < layers) {
+			driver_eink_create_bitplane(img, driver_eink_oldbuf, bit, flags);
+			driver_eink_have_oldbuf = true;
+			continue;
+		}
+
+		int j;
+		for (j = 0; j < p; j++) {
+			int y_start = 0 + j * (DISP_SIZE_Y / p);
+			int y_end = y_start + (DISP_SIZE_Y / p) - 1;
+
+			uint32_t *buf = driver_eink_tmpbuf;
+			driver_eink_create_bitplane(img, buf, bit, flags);
+
+			// clear borders
+			memset_u32(buf, 0, y_start * DISP_SIZE_X_B/4);
+			memset_u32(&buf[(y_end+1) * DISP_SIZE_X_B/4], 0, (DISP_SIZE_Y-y_end-1) * DISP_SIZE_X_B/4);
+
+			struct driver_eink_lut_entry lut[4];
+
+			if (driver_eink_have_oldbuf) {
+				// LUT:
+				//   Use old state as previous layer;
+				//   Do nothing when bits are not set;
+				//   Make pixel whiter when bit is set;
+				//   Duration is <t> cycles.
+				lut[0].length = t;
+				lut[0].voltages = 0x80;
+				lut[1].length = t;
+				lut[1].voltages = 0xa0;
+				lut[2].length = t;
+				lut[2].voltages = 0xa8;
+				lut[3].length = 0;
+
+			} else {
+				// LUT:
+				//   Ignore old state;
+				//   Do nothing when bit is not set;
+				//   Make pixel whiter when bit is set;
+				//   Duration is <t> cycles.
+				lut[0].length = t;
+				lut[0].voltages = 0x88;
+				lut[1].length = 0;
+			}
+
+			/* update display */
+			struct driver_eink_update eink_upd = {
+				.lut        = DRIVER_EINK_LUT_CUSTOM,
+				.lut_custom = lut,
+				.reg_0x3a   = 0, // no dummy lines per gate
+				.reg_0x3b   = 0, // 30us per line
+				.y_start    = y_start,
+				.y_end      = y_end + 1,
+			};
+			driver_eink_update(buf, &eink_upd);
+			driver_eink_have_oldbuf = false;
+		}
+	}
+}
+
+void
+driver_eink_deep_sleep(void)
+{
+	// enter deep sleep
+	driver_eink_dev_write_command_p1(0x10, 0x01);
+}
+
+void
+driver_eink_wakeup(void)
+{
+	// leave deep sleep
+	driver_eink_dev_write_command_p1(0x10, 0x00);
+}
+
+esp_err_t
+driver_eink_init(enum driver_eink_dev_t dev_type)
+{
+	static bool driver_eink_init_done = false;
+
+	if (driver_eink_init_done)
+		return ESP_OK;
+
+	ESP_LOGD(TAG, "init called");
+
+	// initialize spi interface to display
+	esp_err_t res = driver_eink_dev_init(dev_type);
+	if (res != ESP_OK)
+		return res;
+
+	// allocate buffers
+	driver_eink_tmpbuf = heap_caps_malloc(DISP_SIZE_X_B * DISP_SIZE_Y, MALLOC_CAP_32BIT);
+	if (driver_eink_tmpbuf == NULL)
+		return ESP_ERR_NO_MEM;
+
+	if (driver_eink_dev_type == DRIVER_EINK_DEPG0290B1)
+	{
+		driver_eink_oldbuf = heap_caps_malloc(DISP_SIZE_X_B * DISP_SIZE_Y, MALLOC_CAP_32BIT);
+		if (driver_eink_oldbuf == NULL)
+			return ESP_ERR_NO_MEM;
+	}
+
+	if (driver_eink_dev_type == DRIVER_EINK_GDEH029A1)
+	{
+		/* initialize GDEH029A1 */
+
+		// Hardware reset
+		driver_eink_dev_reset();
+
+		// Software reset
+		driver_eink_dev_write_command(0x12);
+
+		// 0C: booster soft start control
+		driver_eink_dev_write_command_p3(0x0c, 0xd7, 0xd6, 0x9d);
+
+		// 2C: write VCOM register
+		driver_eink_dev_write_command_p1(0x2c, 0xa8); // VCOM 7c
+
+		// 11: data entry mode setting
+		driver_eink_dev_write_command_p1(0x11, 0x03); // X inc, Y inc
+	}
+
+	if (driver_eink_dev_type == DRIVER_EINK_DEPG0290B1)
+	{
+		/* initialize DEPG0290B01 */
+
+		// Hardware reset
+		driver_eink_dev_reset();
+
+		// Software reset
+		driver_eink_dev_write_command(0x12);
+
+		// Set analog block control
+		driver_eink_dev_write_command_p1(0x74, 0x54);
+
+		// Set digital block control
+		driver_eink_dev_write_command_p1(0x7E, 0x3B);
+
+		// Set display size and driver output control
+		driver_eink_dev_write_command_p3(0x01, 0x27, 0x01, 0x00);
+
+		// Ram data entry mode
+		// Adress counter is updated in Y direction, Y increment, X increment
+		driver_eink_dev_write_command_p1(0x11, 0x03);
+
+		// Set RAM X address (00h to 0Fh)
+		driver_eink_dev_write_command_p2(0x44, 0x00, 0x0F);
+
+		// Set RAM Y address (0127h to 0000h)
+		driver_eink_dev_write_command_p4(0x45, 0x00, 0x00, 0x27, 0x01);
+
+		// Set border waveform for VBD (see datasheet)
+		driver_eink_dev_write_command_p1(0x3C, 0x01);
+
+		// SET VOLTAGE
+
+		// Set VCOM value
+		driver_eink_dev_write_command_p1(0x2C, 0x26);
+
+		// Gate voltage setting (17h = 20 Volt, ranges from 10v to 21v)
+		driver_eink_dev_write_command_p1(0x03, 0x17);
+
+		// Source voltage setting (15volt, 0 volt and -15 volt)
+		driver_eink_dev_write_command_p3(0x04, 0x41, 0x00, 0x32);
+	}
+
+	driver_eink_init_done = true;
+
+	ESP_LOGD(TAG, "init done");
+
+	return ESP_OK;
+}
+#else
+esp_err_t driver_eink_init(void) { return ESP_OK; } //Dummy function.
+#endif // CONFIG_DRIVER_EINK_ENABLE
---
+++ firmware/components/driver_display_eink/driver_eink_dev.c
@@ -0,0 +1,380 @@
+#include <sdkconfig.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <rom/ets_sys.h>
+#include <rom/gpio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+#include <string.h>
+#include <soc/gpio_reg.h>
+#include <soc/gpio_sig_map.h>
+#include <soc/gpio_struct.h>
+#include <soc/spi_reg.h>
+#include <esp_err.h>
+#include <esp_log.h>
+#include <driver/gpio.h>
+#include <driver/spi_master.h>
+#include <esp_heap_caps.h>
+
+#include "driver_pins.h"
+#include "driver_base.h"
+#include "driver_spi.h"
+#include "driver_eink_dev.h"
+
+#ifndef LOW
+#define LOW 0
+#endif
+#ifndef HIGH
+#define HIGH 1
+#endif
+
+#ifndef VSPICLK_OUT_IDX
+// use old define
+#define VSPICLK_OUT_IDX VSPICLK_OUT_MUX_IDX
+#endif
+
+// this number should be a multiply of 4
+// to transfer complete dwords in one go
+#define SPI_TRANSFER_SIZE 128
+
+static const char *TAG = "driver_eink_dev";
+
+enum driver_eink_dev_t driver_eink_dev_type = DRIVER_EINK_DEFAULT;
+
+#ifdef PIN_NUM_EPD_RESET
+
+static spi_device_handle_t spi_bus = NULL;
+
+// forward declarations
+static esp_err_t driver_eink_dev_release_spi(void);
+static void driver_spi_pre_transfer_callback(spi_transaction_t *t);
+
+static esp_err_t
+driver_eink_dev_claim_spi(void)
+{
+	// already claimed?
+	if (spi_bus != NULL) return ESP_OK;
+
+	ESP_LOGI(TAG, "claiming VSPI bus");
+	driver_vspi_release_and_claim(driver_eink_dev_release_spi);
+
+	static const spi_bus_config_t buscfg = {
+		.mosi_io_num     = PIN_NUM_EPD_MOSI,
+		.miso_io_num     = -1, // MISO is not used, we are transferring to the slave only
+		.sclk_io_num     = PIN_NUM_EPD_CLK,
+		.quadwp_io_num   = -1,
+		.quadhd_io_num   = -1,
+		.max_transfer_sz = SPI_TRANSFER_SIZE,
+	};
+
+	esp_err_t res = spi_bus_initialize(VSPI_HOST, &buscfg, 2);
+	assert(res == ESP_OK);
+
+	static const spi_device_interface_config_t devcfg = {
+		.clock_speed_hz = 20 * 1000 * 1000,
+		.mode           = 0,  // SPI mode 0
+		.spics_io_num   = PIN_NUM_EPD_CS,
+		.queue_size     = 1,
+		.flags          = (SPI_DEVICE_HALFDUPLEX | SPI_DEVICE_3WIRE), // We are sending only in one direction (to the ePaper slave)
+		.pre_cb         = driver_spi_pre_transfer_callback,            // Specify pre-transfer callback to handle D/C line
+	};
+
+	res = spi_bus_add_device(VSPI_HOST, &devcfg, &spi_bus);
+	assert(res == ESP_OK);
+
+	ESP_LOGI(TAG, "claiming VSPI bus: done");
+	return res;
+}
+
+static esp_err_t
+driver_eink_dev_release_spi(void)
+{
+	ESP_LOGI(TAG, "releasing VSPI bus");
+	esp_err_t res = spi_bus_remove_device(spi_bus);
+	assert(res == ESP_OK);
+	spi_bus = NULL;
+
+	res = spi_bus_free(VSPI_HOST);
+	assert(res == ESP_OK);
+
+	driver_vspi_freed();
+
+	ESP_LOGI(TAG, "releasing VSPI bus: done");
+	return ESP_OK;
+}
+
+esp_err_t
+driver_eink_dev_reset(void) {
+	esp_err_t res = gpio_set_level(PIN_NUM_EPD_RESET, LOW);
+	if (res != ESP_OK)
+		return res;
+
+	ets_delay_us(200000);
+
+	res = gpio_set_level(PIN_NUM_EPD_RESET, HIGH);
+	if (res != ESP_OK)
+		return res;
+
+	ets_delay_us(200000);
+
+	return ESP_OK;
+}
+
+bool
+driver_eink_dev_is_busy(void)
+{
+	return gpio_get_level(PIN_NUM_EPD_BUSY);
+}
+
+// semaphore to trigger on gde-busy signal
+xSemaphoreHandle driver_eink_dev_intr_trigger = NULL;
+
+void
+driver_eink_dev_busy_wait(void)
+{
+	while (driver_eink_dev_is_busy())
+	{
+		xSemaphoreTake(driver_eink_dev_intr_trigger, 100 / portTICK_PERIOD_MS);
+	}
+}
+
+void
+driver_eink_dev_intr_handler(void *arg)
+{ /* in interrupt handler */
+#ifdef CONFIG_DRIVER_EINK_DEBUG
+	static int gpio_last_state = -1;
+	if (gpio_state != -1 && gpio_last_state != gpio_state)
+	{
+		ets_printf("driver_eink_dev: EPD-Busy Int %s\n", gpio_state == 0 ? "up" : "down");
+	}
+	gpio_last_state = gpio_state;
+#endif // CONFIG_DRIVER_EINK_DEBUG
+
+//	if (gpio_state == 0)
+//	{
+		xSemaphoreGiveFromISR(driver_eink_dev_intr_trigger, NULL);
+//	}
+}
+
+
+/* This function is called (in irq context!) just before a transmission starts.
+ * It will set the D/C line to the value indicated in the user field
+ */
+static
+void driver_spi_pre_transfer_callback(spi_transaction_t *t)
+{
+	uint8_t dc_level = *((uint8_t *) t->user);
+	gpio_set_level(PIN_NUM_EPD_DATA, (int) dc_level);
+}
+
+
+void
+driver_spi_send(const uint8_t *data, int len, const uint8_t dc_level)
+{
+	if (len == 0) {
+		return;
+	}
+
+	esp_err_t ret = driver_eink_dev_claim_spi();
+	assert(ret == ESP_OK);
+
+	spi_transaction_t t = {
+		.length = len * 8,  // transaction length is in bits
+		.tx_buffer = data,
+		.user = (void *) &dc_level,
+	};
+	ret = spi_device_transmit(spi_bus, &t);
+	assert(ret == ESP_OK);
+}
+
+void
+driver_eink_dev_write_command(uint8_t command)
+{
+	driver_eink_dev_busy_wait();
+	driver_spi_send(&command, 1, LOW);
+}
+
+void
+driver_eink_dev_write_byte(uint8_t data)
+{
+	driver_eink_dev_busy_wait();
+	driver_spi_send(&data, 1, HIGH);
+}
+
+void
+driver_eink_dev_write_command_stream(uint8_t command, const uint8_t *data,
+										 unsigned int datalen)
+{
+	ESP_LOGI(TAG, "Sending SPI stream of %d bytes", datalen);
+	driver_eink_dev_write_command(command);
+	driver_spi_send(data, datalen, HIGH);
+	ESP_LOGI(TAG, "Done");
+}
+
+/* This function is dedicated to copy memory allocated with MALLOC_CAP_32BIT
+ * Such a memory can only be accessed via 32-bit reads and writes,
+ * any other type of access will generate a fatal LoadStoreError exception.
+ */
+static void
+memcpy_u8_u32(uint8_t *dst, const uint32_t *src, size_t size)
+{
+	while (size-- > 0)
+	{
+		uint32_t data_src = *src++;
+		*dst++ = data_src >> 24;
+		*dst++ = data_src >> 16;
+		*dst++ = data_src >> 8;
+		*dst++ = data_src;
+	}
+}
+
+/* Send uint32_t stream in chunks of SPI_TRANSFER_SIZE
+ * to use to the maximum the size of memory allocated in the SPI buffer
+ */
+void
+driver_eink_dev_write_command_stream_u32(uint8_t command, const uint32_t *data,
+										 unsigned int datalen)
+{
+	assert(SPI_TRANSFER_SIZE % 4 == 0);
+
+	uint8_t* data_tmpbuf = heap_caps_malloc(SPI_TRANSFER_SIZE, MALLOC_CAP_8BIT);
+	ESP_LOGI(TAG, "Sending SPI stream of %d dwords...", datalen);
+	if (data_tmpbuf == NULL){
+		ESP_LOGE(TAG, "Failed to allocate memory!");
+	}
+	while (datalen >= SPI_TRANSFER_SIZE / 4){
+		memcpy_u8_u32(data_tmpbuf, data, SPI_TRANSFER_SIZE / 4);
+		driver_eink_dev_write_command(command);
+		driver_spi_send(data_tmpbuf, SPI_TRANSFER_SIZE, HIGH);
+		data += SPI_TRANSFER_SIZE / 4;
+		datalen -= SPI_TRANSFER_SIZE / 4;
+	}
+	// send remaining data if < SPI_TRANSFER_SIZE / 4
+	if (datalen > 0) {
+		memcpy_u8_u32(data_tmpbuf, data, datalen);
+		driver_eink_dev_write_command(command);
+		driver_spi_send(data_tmpbuf, datalen*4, HIGH);
+	}
+	heap_caps_free(data_tmpbuf);
+	ESP_LOGI(TAG, "Done");
+}
+
+esp_err_t
+driver_eink_dev_init(enum driver_eink_dev_t dev_type)
+{
+	static bool driver_eink_dev_init_done = false;
+
+	if (driver_eink_dev_init_done)
+		return ESP_OK;
+
+	ESP_LOGD(TAG, "init called");
+
+	// initialize VSPI sharing
+	driver_vspi_init();
+
+	driver_eink_dev_type = dev_type;
+
+	esp_err_t res = driver_base_init();
+	if (res != ESP_OK)
+		return res;
+
+#ifdef PIN_NUM_LED
+	gpio_pad_select_gpio(PIN_NUM_LED);
+	res = gpio_set_direction(PIN_NUM_LED, GPIO_MODE_OUTPUT);
+	if (res != ESP_OK)
+		return res;
+#endif // PIN_NUM_LED
+
+	driver_eink_dev_intr_trigger = xSemaphoreCreateBinary();
+	if (driver_eink_dev_intr_trigger == NULL)
+		return ESP_ERR_NO_MEM;
+
+	res = gpio_isr_handler_add(PIN_NUM_EPD_BUSY, driver_eink_dev_intr_handler, NULL);
+	if (res != ESP_OK)
+		return res;
+
+	gpio_config_t io_conf = {
+		.intr_type    = GPIO_INTR_ANYEDGE,
+		.mode         = GPIO_MODE_INPUT,
+		.pin_bit_mask = 1LL << PIN_NUM_EPD_BUSY,
+		.pull_down_en = 0,
+		.pull_up_en   = 1,
+	};
+	res = gpio_config(&io_conf);
+	if (res != ESP_OK)
+		return res;
+
+	res = gpio_set_direction(PIN_NUM_EPD_CS, GPIO_MODE_OUTPUT);
+	if (res != ESP_OK)
+		return res;
+
+	res = gpio_set_direction(PIN_NUM_EPD_DATA, GPIO_MODE_OUTPUT);
+	if (res != ESP_OK)
+		return res;
+
+	res = gpio_set_direction(PIN_NUM_EPD_RESET, GPIO_MODE_OUTPUT);
+	if (res != ESP_OK)
+		return res;
+
+	res = gpio_set_direction(PIN_NUM_EPD_BUSY, GPIO_MODE_INPUT);
+	if (res != ESP_OK)
+		return res;
+
+
+	driver_eink_dev_init_done = true;
+
+	ESP_LOGD(TAG, "init done");
+
+	return ESP_OK;
+}
+
+#else
+
+// add dummy functions
+esp_err_t
+driver_eink_dev_reset(void) {
+	return ESP_OK;
+}
+
+bool
+driver_eink_dev_is_busy(void)
+{
+	return false;
+}
+
+void
+driver_eink_dev_busy_wait(void)
+{
+}
+
+void
+driver_eink_dev_write_command(uint8_t command)
+{
+}
+
+
+esp_err_t
+driver_eink_dev_init(enum driver_eink_dev_t dev_type)
+{
+	return ESP_OK;
+}
+
+void
+driver_eink_dev_write_byte(uint8_t data)
+{
+}
+
+void
+driver_eink_dev_write_command_stream(uint8_t command, const uint8_t *data,
+										 unsigned int datalen)
+{
+}
+
+void
+driver_eink_dev_write_command_stream_u32(uint8_t command, const uint32_t *data,
+										 unsigned int datalen)
+{
+}
+
+#endif // PIN_NUM_EPD_RESET
