7570c4ded5bccbfc63fb5aaab6abbd57085b65eb
Renze Nicolai <renze@rnplus.nl>
2019-07-14 22:46:34 +0200

    Add framebuffer to platform init

--- firmware/main/platform.c
+++ firmware/main/platform.c
@@ -21,10 +21,11 @@ void platform_init()
 	INIT_DRIVER(vspi)         //Generic VSPI bus driver
 	INIT_DRIVER(i2c)          //Generic I2C bus driver
 	INIT_DRIVER(hub75)        //LED matrix as found on the CampZone 2019 badge
-	INIT_DRIVER(mpr121)       //I/O expander with touch inputs as found on the SHA2017 and HackerHotel 2019 badges
-	INIT_DRIVER(neopixel)     //Addressable LEDs as found on the SHA2017 and HackerHotel 2019 badges
 	INIT_DRIVER(erc12864)     //128x64 LCD screen as found on the Disobey 2019 badge
 	INIT_DRIVER(ssd1306)      //128x64 OLED screen as found on the Disobey 2020 badge
 	INIT_DRIVER(eink)         //296x128 e-ink display as found on the SHA2017 and HackerHotel 2019 badges
+	INIT_DRIVER(framebuffer)  //Framebuffer driver with basic drawing routines
+	INIT_DRIVER(mpr121)       //I/O expander with touch inputs as found on the SHA2017 and HackerHotel 2019 badges
 	INIT_DRIVER(disobey_samd) //I/O via the SAMD co-processor on the Disobey 2019 badge
+	INIT_DRIVER(neopixel)     //Addressable LEDs as found on the SHA2017 and HackerHotel 2019 badges
 }
