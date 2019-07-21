1f7fcb4af134b11461c3d0ea8493b6c41f118fdc
Renze Nicolai <renze@rnplus.nl>
2019-07-22 00:10:35 +0200

    Switch SHA2017 to shared modules

--- firmware/python_modules/sha2017/dashboard/terminal/nickname.py
+++ firmware/python_modules/sha2017/dashboard/terminal/nickname.py
@@ -1,9 +1,1 @@
-import term, system, badge, easydraw
-
-system.serialWarning()
-
-term.header(True, "Configure nickname")
-nickname = badge.nvs_get_str("owner", "name", "")
-nickname = term.prompt("Nickname", 1, 3, nickname)
-badge.nvs_set_str("owner", "name", nickname)
-system.home()
+../../../shared/dashboard/terminal/nickname.py
