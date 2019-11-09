3461d2288acf66f27ece55574f877e4cd5fb8476
Renze Nicolai <renze@rnplus.nl>
2019-11-09 13:29:16 +0100

    Prevent compilation error when framebuffer is disabled

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -14,15 +14,19 @@ void app_main()
 	platform_init();
 
 	if (is_first_boot) {
-		driver_framebuffer_fill(NULL, COLOR_BLACK);
-		driver_framebuffer_print(NULL, "Extracting ZIP...\n", 0, 0, 1, 1, COLOR_WHITE, &roboto12pt7b);
-		driver_framebuffer_flush(0);
+		#ifdef CONFIG_DRIVER_FRAMEBUFFER_ENABLE
+			driver_framebuffer_fill(NULL, COLOR_BLACK);
+			driver_framebuffer_print(NULL, "Extracting ZIP...\n", 0, 0, 1, 1, COLOR_WHITE, &roboto12pt7b);
+			driver_framebuffer_flush(0);
+		#endif
 		printf("Attempting to unpack FAT initialization ZIP file...\b");
 		if (unpack_first_boot_zip() != ESP_OK) { //Error
-			driver_framebuffer_fill(NULL, COLOR_BLACK);
-			driver_framebuffer_print(NULL, "ZIP error\nRESET TO SKIP\n", 0, 0, 1, 1, COLOR_WHITE, &roboto12pt7b);
-			driver_framebuffer_flush(0);
-			printf("An error occured while unpacking the ZIP file!\b");
+			#ifdef CONFIG_DRIVER_FRAMEBUFFER_ENABLE
+				driver_framebuffer_fill(NULL, COLOR_BLACK);
+				driver_framebuffer_print(NULL, "ZIP error\nRESET TO SKIP\n", 0, 0, 1, 1, COLOR_WHITE, &roboto12pt7b);
+				driver_framebuffer_flush(0);
+			#endif
+			printf("An error occured while unpacking the ZIP file!\nReset the board to continue without provisioning.\b");
 		} else { //Done
 			esp_restart();
 		}
