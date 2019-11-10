ad5a18f8993516d78f9ce59edfee0d911ad5dc2c
Renze Nicolai <renze@rnplus.nl>
2019-11-10 01:47:19 +0100

    Fix first-boot issues caused by wrong assumptions :-(

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -23,22 +23,20 @@ void app_main()
 		if (unpack_first_boot_zip() != ESP_OK) { //Error
 			#ifdef CONFIG_DRIVER_FRAMEBUFFER_ENABLE
 				driver_framebuffer_fill(NULL, COLOR_BLACK);
-				driver_framebuffer_print(NULL, "ZIP error\nRESET TO SKIP\n", 0, 0, 1, 1, COLOR_WHITE, &roboto12pt7b);
+				driver_framebuffer_print(NULL, "ZIP error!\n", 0, 0, 1, 1, COLOR_WHITE, &roboto12pt7b);
 				driver_framebuffer_flush(0);
 			#endif
 			printf("An error occured while unpacking the ZIP file!\nReset the board to continue without provisioning.\b");
-		} else { //Done
-			esp_restart();
+			halt();
 		}
-	} else {
-		int magic = get_magic();
-		
-		switch(magic) {
-			case MAGIC_OTA:
-				badge_ota_update();
-				break;
-			default:
-				micropython_entry();
-		}
+	}
+	int magic = get_magic();
+	
+	switch(magic) {
+		case MAGIC_OTA:
+			badge_ota_update();
+			break;
+		default:
+			micropython_entry();
 	}
 }
