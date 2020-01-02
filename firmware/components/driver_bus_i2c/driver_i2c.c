f0d0f7752a9e7a101a861cb36b042c546a96f1fe
Renze Nicolai <renze@rnplus.nl>
2020-01-03 00:18:22 +0100

    Replace the asserts in the i2c driver with return-on-error statements.

--- firmware/components/driver_bus_i2c/driver_i2c.c
+++ firmware/components/driver_bus_i2c/driver_i2c.c
@@ -16,12 +16,12 @@
 #define I2C_MASTER_TX_BUF_DISABLE  0
 #define I2C_MASTER_RX_BUF_DISABLE  0
 
-#define WRITE_BIT      I2C_MASTER_WRITE /*!< I2C master write */
-#define READ_BIT       I2C_MASTER_READ  /*!< I2C master read */
-#define ACK_CHECK_EN   0x1     /*!< I2C master will check ack from slave */
-#define ACK_CHECK_DIS  0x0     /*!< I2C master will not check ack from slave */
-#define ACK_VAL        0x0     /*!< I2C ack value */
-#define NACK_VAL       0x1     /*!< I2C nack value */
+#define WRITE_BIT      I2C_MASTER_WRITE // I2C master write
+#define READ_BIT       I2C_MASTER_READ  // I2C master read
+#define ACK_CHECK_EN   0x1              // I2C master will check ack from slave
+#define ACK_CHECK_DIS  0x0              // I2C master will not check ack from slave
+#define ACK_VAL        0x0              // I2C ack value
+#define NACK_VAL       0x1              // I2C nack value
 
 static const char *TAG = "driver_i2c";
 
@@ -69,228 +69,235 @@ esp_err_t driver_i2c_init(void)
 esp_err_t driver_i2c_read_reg(uint8_t addr, uint8_t reg, uint8_t *value, size_t value_len)
 {
 	esp_err_t res;
-	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE)
+	
+	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE) {
+		ESP_LOGD(TAG, "i2c bus is busy");
 		return ESP_ERR_TIMEOUT;
+	}
 
 	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
 	res = i2c_master_start(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, ( addr << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 
 	res = i2c_master_start(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, ( addr << 1 ) | READ_BIT, ACK_CHECK_EN);
-	assert( res == ESP_OK );
-	if (value_len > 1)
-	{
+	if ( res != ESP_OK ) return res;
+	
+	if (value_len > 1) {
 		res = i2c_master_read(cmd, value, value_len-1, ACK_VAL);
-		assert( res == ESP_OK );
+		if ( res != ESP_OK ) return res;
 	}
 	res = i2c_master_read_byte(cmd, &value[value_len-1], NACK_VAL);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_stop(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 
 	res = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
 	i2c_cmd_link_delete(cmd);
 
-	if (xSemaphoreGive(driver_i2c_mux) != pdTRUE)
-	{
-		ESP_LOGE(TAG, "xSemaphoreGive() did not return pdTRUE.");
-	}
-
+	xSemaphoreGive(driver_i2c_mux);
 	return res;
 }
 
 esp_err_t driver_i2c_write_byte(uint8_t addr, uint8_t value)
 {
 	esp_err_t res;
-	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE)
+	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE) {
+		ESP_LOGD(TAG, "i2c bus is busy");
 		return ESP_ERR_TIMEOUT;
+	}
 
 	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
 	res = i2c_master_start(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, ( addr << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, value, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_stop(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 
 	res = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
 	i2c_cmd_link_delete(cmd);
 
-	if (xSemaphoreGive(driver_i2c_mux) != pdTRUE)
-	{
-		ESP_LOGE(TAG, "xSemaphoreGive() did not return pdTRUE.");
-	}
-
+	xSemaphoreGive(driver_i2c_mux);
 	return res;
 }
 
 esp_err_t driver_i2c_write_reg(uint8_t addr, uint8_t reg, uint8_t value)
 {
 	esp_err_t res;
-	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE)
+	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE) {
+		ESP_LOGD(TAG, "i2c bus is busy");
 		return ESP_ERR_TIMEOUT;
+	}
 
 	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
 	res = i2c_master_start(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, ( addr << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, value, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_stop(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 
 	res = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
 	i2c_cmd_link_delete(cmd);
 
-	if (xSemaphoreGive(driver_i2c_mux) != pdTRUE)
-	{
-		ESP_LOGE(TAG, "xSemaphoreGive() did not return pdTRUE.");
-	}
-
+	xSemaphoreGive(driver_i2c_mux);
 	return res;
 }
 
 esp_err_t driver_i2c_write_buffer(uint8_t addr, const uint8_t* buffer, uint16_t len)
 {
 	esp_err_t res;
-	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE)
+	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE) {
+		ESP_LOGD(TAG, "i2c bus is busy");
 		return ESP_ERR_TIMEOUT;
+	}
 
 	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
 	res = i2c_master_start(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, ( addr << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	for (uint16_t i = 0; i < len; i++) {
-	res = i2c_master_write_byte(cmd, buffer[i], ACK_CHECK_EN);
-	assert( res == ESP_OK );
-}
+		res = i2c_master_write_byte(cmd, buffer[i], ACK_CHECK_EN);
+		if ( res != ESP_OK ) return res;
+	}
 
 	res = i2c_master_stop(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 
 	res = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
 	i2c_cmd_link_delete(cmd);
 
-	if (xSemaphoreGive(driver_i2c_mux) != pdTRUE)
-	{
-		ESP_LOGE(TAG, "xSemaphoreGive() did not return pdTRUE.");
-	}
-
+	xSemaphoreGive(driver_i2c_mux);
 	return res;
 }
 
 esp_err_t driver_i2c_write_buffer_reg(uint8_t addr, uint8_t reg, const uint8_t* buffer, uint16_t len)
 {
 	esp_err_t res;
-	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE)
+	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE) {
+		ESP_LOGD(TAG, "i2c bus is busy");
 		return ESP_ERR_TIMEOUT;
+	}
 
 	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
 	res = i2c_master_start(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, ( addr << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	for (uint16_t i = 0; i < len; i++) {
-	res = i2c_master_write_byte(cmd, buffer[i], ACK_CHECK_EN);
-	assert( res == ESP_OK );
-}
+		res = i2c_master_write_byte(cmd, buffer[i], ACK_CHECK_EN);
+		if ( res != ESP_OK ) return res;
+	}
 
 	res = i2c_master_stop(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 
 	res = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
 	i2c_cmd_link_delete(cmd);
 
-	if (xSemaphoreGive(driver_i2c_mux) != pdTRUE)
-	{
-		ESP_LOGE(TAG, "xSemaphoreGive() did not return pdTRUE.");
-	}
-
+	xSemaphoreGive(driver_i2c_mux);
 	return res;
 }
 
 esp_err_t driver_i2c_write_reg32(uint8_t addr, uint8_t reg, uint32_t value)
 {
 	esp_err_t res;
-	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE)
+	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE) {
+		ESP_LOGD(TAG, "i2c bus is busy");
 		return ESP_ERR_TIMEOUT;
+	}
 
 	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
 	res = i2c_master_start(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 	res = i2c_master_write_byte(cmd, ( addr << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 	res = i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 	res = i2c_master_write_byte(cmd, (value)&0xFF, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 	res = i2c_master_write_byte(cmd, (value>>8)&0xFF, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 	res = i2c_master_write_byte(cmd, (value>>16)&0xFF, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 	res = i2c_master_write_byte(cmd, (value>>24)&0xFF, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 	res = i2c_master_stop(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 
 	res = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
 	i2c_cmd_link_delete(cmd);
 
-	if (xSemaphoreGive(driver_i2c_mux) != pdTRUE)
-	{
-		ESP_LOGE(TAG, "xSemaphoreGive() did not return pdTRUE.");
-	}
-
+	xSemaphoreGive(driver_i2c_mux);
 	return res;
 }
 
 esp_err_t driver_i2c_read_event(uint8_t addr, uint8_t *buf)
 {
 	esp_err_t res;
-	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE)
+	if (xSemaphoreTake(driver_i2c_mux, portMAX_DELAY) != pdTRUE) {
+		ESP_LOGD(TAG, "i2c bus is busy");
 		return ESP_ERR_TIMEOUT;
+	}
 
 	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
 	res = i2c_master_start(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_write_byte(cmd, ( addr << 1 ) | READ_BIT, ACK_CHECK_EN);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_read(cmd, buf, 2, ACK_VAL);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_read_byte(cmd, &buf[2], NACK_VAL);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
+	
 	res = i2c_master_stop(cmd);
-	assert( res == ESP_OK );
+	if ( res != ESP_OK ) return res;
 
 	res = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
 	i2c_cmd_link_delete(cmd);
 
-	if (xSemaphoreGive(driver_i2c_mux) != pdTRUE)
-	{
-		ESP_LOGE(TAG, "xSemaphoreGive() did not return pdTRUE.");
-	}
-
+	xSemaphoreGive(driver_i2c_mux);
 	return res;
 }
 
