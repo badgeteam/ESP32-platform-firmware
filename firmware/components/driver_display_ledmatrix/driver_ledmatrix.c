b8638f7bd10e6eb44b6517490f83e99bde0e1b59
Renze Nicolai <renze@rnplus.nl>
2020-01-12 14:42:20 +0100

    Ledmatrix driver: first working version

--- firmware/components/driver_display_ledmatrix/driver_ledmatrix.c
+++ firmware/components/driver_display_ledmatrix/driver_ledmatrix.c
@@ -18,38 +18,87 @@
 
 #include "include/driver_ledmatrix.h"
 
-#define LEDMATRIX_TASK_STACK_LEN 1024
-#define LEDMATRIX_TASK_PRIORITY  5
-#define LEDMATRIX_TASK_CPU       1
-
 #ifdef CONFIG_DRIVER_LEDMATRIX_ENABLE
 #define TAG "ledmatrix"
 
-void ledmatrix_display_task (void* arg)
-{	
-    while (1) {
-	    //to-do: gpio flipping here :P
-		vTaskDelay(1 / portTICK_PERIOD_MS); //Wait 1ms
-	}
+//uint8_t rate = 60;
 
+const uint8_t rows[] = {DRIVER_LEDMATRIX_ROWS};
+const uint8_t columns[] = {DRIVER_LEDMATRIX_COLUMNS};
+
+const uint8_t* buffer;
+
+void displayTask(void* arg)
+{
+	while (1) {
+		for (uint8_t s = 0; s < 0xFF; s++) {
+			for (uint8_t c = 0; c < sizeof(columns); c++) {
+				//TickType_t xLastWakeTime = xTaskGetTickCount();
+				bool any = false;
+				for (uint8_t r = 0; r < sizeof(rows); r++) {
+					bool state = buffer && (buffer[r*sizeof(columns) + c] > s);
+					if (state) any = true;
+					gpio_set_level(rows[r], state);
+				}
+				if (any) gpio_set_level(columns[c], true);
+				vTaskDelay(1 / portTICK_PERIOD_MS);
+				//vTaskDelayUntil( &xLastWakeTime, 1.0 / rate * 1000 / portTICK_PERIOD_MS );
+				gpio_set_level(columns[c], false);
+				//vTaskDelay(1 / portTICK_PERIOD_MS);
+			}
+		}
+	}
 }
 
 esp_err_t driver_ledmatrix_init(void)
 {
+	// Use a static boolean to make sure we only initialise the driver once
 	static bool driver_ledmatrix_init_done = false;
 	if (driver_ledmatrix_init_done) return ESP_OK;
 	ESP_LOGD(TAG, "init called");
 	
-	//to-do: gpio init
+	esp_err_t res;
 	
-#ifdef LEDMATRIX_TASK_CPU
-	xTaskCreatePinnedToCore(ledmatrix_display_task, "ledmatrix", LEDMATRIX_TASK_STACK_LEN, NULL, LEDMATRIX_TASK_PRIORITY, NULL, LEDMATRIX_TASK_CPU);
-#else
-	xTaskCreate(ledmatrix_display_task, "ledmatrix", LEDMATRIX_TASK_STACK_LEN, NULL, LEDMATRIX_TASK_PRIORITY, NULL);
-#endif
+	// Initialise the GPIO pins used as row outputs
+	for (uint8_t i = 0; i < sizeof(rows); i++) {
+		uint8_t pin = rows[i];
+		printf("[LM] Using pin %u as row output #%u.\n", pin, i);
+		gpio_pad_select_gpio(pin);
+		res = gpio_set_direction(pin, GPIO_MODE_OUTPUT);
+		if (res != ESP_OK) return res;
+		gpio_set_level(pin, false);
+	}
 	
+	// Initialise the GPIO pins used as column outputs
+	for (uint8_t i = 0; i < sizeof(columns); i++) {
+		uint8_t pin = columns[i];
+		printf("[LM] Using pin %u as column output #%u.\n", pin, i);
+		gpio_pad_select_gpio(pin);
+		res = gpio_set_direction(pin, GPIO_MODE_OUTPUT);
+		if (res != ESP_OK) return res;
+		gpio_set_level(pin, false);
+	}
+	
+	// Create a task pinned to core 1 which will handle writing to the display
+	xTaskCreatePinnedToCore(
+		&displayTask, /* Task function. */
+		"ledmatrix",  /* String with name of task. */
+		1024,         /* Stack size in words. */
+		NULL,         /* Parameter passed as input of the task */
+		1,            /* Priority of the task. (Lower = more important) */
+		NULL,         /* Task handle. */
+		1             /* Core ID */
+	);
+	
+	// Set the static boolean indicating that we completed initialisation of the driver
 	driver_ledmatrix_init_done = true;
 	ESP_LOGD(TAG, "init done");
+	return ESP_OK;
+}
+
+esp_err_t driver_ledmatrix_set_buffer(const uint8_t *newBuffer)
+{
+	buffer = newBuffer;
 	return ESP_OK;
 }
 
