8714679ade4d1f5e9bdcf08df4b346447b9b9d73
Tom <mr@tomclement.nl>
2020-03-16 12:19:25 +0100

    Made otadata reset script intelligently read the correct partition size and offset from the current partition definition

--- reset_bl_to_ota0.sh
+++ reset_bl_to_ota0.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 export PATH="$PATH:$(pwd)/xtensa-esp32-elf/bin"
-python2 esp-idf/components/esptool_py/esptool/esptool.py --port `ls /dev/tty{USB0,.wchusbserial*,.SLAB_USBtoUART,ACMx} 2>/dev/null` erase_region 0xd000 0x2000
+
+# Load config variables so we know about the currently selected badge
+source firmware/sdkconfig
+
+# Read partition information from partition table CSV
+OFFSET="$(python scripts/print_partition_info.py firmware/$CONFIG_PARTITION_TABLE_CUSTOM_FILENAME otadata offset)"
+SIZE="$(python scripts/print_partition_info.py firmware/$CONFIG_PARTITION_TABLE_CUSTOM_FILENAME otadata size)"
+
+# Erase otadata partition
+python2 esp-idf/components/esptool_py/esptool/esptool.py --port \
+  `ls /dev/tty{USB0,.wchusbserial*,.SLAB_USBtoUART,ACMx} 2>/dev/null` \
+  erase_region $OFFSET $SIZE
