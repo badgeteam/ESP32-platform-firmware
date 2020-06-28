9e1908ad8877417b44c0e4d83c125d00390bdbc5
Tom <mr@tomclement.nl>
2020-06-28 10:29:03 +0200

    Latest initial FS flashing script (non-working)

--- write_initial_fs.sh
+++ write_initial_fs.sh
@@ -5,19 +5,22 @@ export PATH="$PATH:$(pwd)/xtensa-esp32-elf/bin"
 source firmware/sdkconfig
 
 # Read partition information from partition table CSV
-OFFSET="$(python scripts/print_partition_info.py firmware/$CONFIG_PARTITION_TABLE_CUSTOM_FILENAME locfd offset)"
-SIZE="$(python scripts/print_partition_info.py firmware/$CONFIG_PARTITION_TABLE_CUSTOM_FILENAME locfd size)"
+OFFSET="$(python3 scripts/print_partition_info.py firmware/$CONFIG_PARTITION_TABLE_CUSTOM_FILENAME locfd offset)"
+SIZE="$(python3 scripts/print_partition_info.py firmware/$CONFIG_PARTITION_TABLE_CUSTOM_FILENAME locfd size)"
+
+echo "Size: "$SIZE
+echo "Offset: "$OFFSET
 
 # Prepare FAT image
 cd firmware/components/mkfatfs/src
 make
-./mkfatfs -c ../../../../initial_filesystems/$CONFIG_INFO_HARDWARE_FOLDER -s $SIZE ../../../../initial_fs.img
+./mkfatfs -c ../../../../initial_filesystems/$CONFIG_INFO_HARDWARE_FOLDER -s $SIZE ../../../../initial_fs.img -d 2
 cd ../../../../
 
 # Flash image to ESP32
-#python2 esp-idf/components/esptool_py/esptool/esptool.py --port \
-#  $(bash -c 'ls /dev/tty{USB0,.wchusbserial*,.usbmodem*,.SLAB_USBtoUART,ACMx} 2>/dev/null') \
-#  write_flash $OFFSET initial_fs.img
+python3 esp-idf/components/esptool_py/esptool/esptool.py --port \
+  $(bash -c 'ls /dev/tty{USB*,.wchusbserial*,.usbmodem*,.SLAB_USBtoUART,ACM*} 2>/dev/null') \
+  write_flash $OFFSET fs.dump #initial_fs.img
 
 # And clean up again
 #rm initial_fs.img
