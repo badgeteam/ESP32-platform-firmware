c05178c6e3c02b15fe4ed66993a1f9c679910063
Tom <mr@tomclement.nl>
2020-06-28 11:19:13 +0200

    Changed write_initial_fs.sh to use old ZIP approach, since building a FATFS image that gets recognised by ESP drivers is not yet working

--- write_initial_fs.sh
+++ write_initial_fs.sh
@@ -5,22 +5,20 @@ export PATH="$PATH:$(pwd)/xtensa-esp32-elf/bin"
 source firmware/sdkconfig
 
 # Read partition information from partition table CSV
-OFFSET="$(python3 scripts/print_partition_info.py firmware/$CONFIG_PARTITION_TABLE_CUSTOM_FILENAME locfd offset)"
-SIZE="$(python3 scripts/print_partition_info.py firmware/$CONFIG_PARTITION_TABLE_CUSTOM_FILENAME locfd size)"
+OFFSET="$(python3 scripts/print_partition_info.py firmware/$CONFIG_PARTITION_TABLE_CUSTOM_FILENAME ota_1 offset)"
+SIZE="$(python3 scripts/print_partition_info.py firmware/$CONFIG_PARTITION_TABLE_CUSTOM_FILENAME ota_1 size)"
 
-echo "Size: "$SIZE
-echo "Offset: "$OFFSET
+# Prepare zip file
+cd initial_filesystems/$CONFIG_INFO_HARDWARE_FOLDER
+zip -r initial_fs.zip . -x "*.DS_Store"
+cd ../..
 
-# Prepare FAT image
-cd firmware/components/mkfatfs/src
-make
-./mkfatfs -c ../../../../initial_filesystems/$CONFIG_INFO_HARDWARE_FOLDER -s $SIZE ../../../../initial_fs.img -d 2
-cd ../../../../
+# TODO: check if zip fits in ota partition
 
-# Flash image to ESP32
+# Flash zip file to the OTA partition that it's not booting from
 python3 esp-idf/components/esptool_py/esptool/esptool.py --port \
   $(bash -c 'ls /dev/tty{USB*,.wchusbserial*,.usbmodem*,.SLAB_USBtoUART,ACM*} 2>/dev/null') \
-  write_flash $OFFSET fs.dump #initial_fs.img
+  write_flash $OFFSET initial_filesystems/$CONFIG_INFO_HARDWARE_FOLDER/initial_fs.zip
 
 # And clean up again
-#rm initial_fs.img
+rm initial_filesystems/$CONFIG_INFO_HARDWARE_FOLDER/initial_fs.zip
