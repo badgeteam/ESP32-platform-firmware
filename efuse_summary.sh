8ef6b6e10253957c18b84f594aad2477644a442c
Tom <mr@tomclement.nl>
2020-06-28 10:27:32 +0200

    Bumped build scripts to python3

--- efuse_summary.sh
+++ efuse_summary.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python2 esp-idf/components/esptool_py/esptool/espefuse.py --port $(bash -c 'ls /dev/tty{USB0,.wchusbserial*,.usbmodem*,.SLAB_USBtoUART,ACMx} 2>/dev/null') --baud 115200 summary
+python3 esp-idf/components/esptool_py/esptool/espefuse.py --port $(bash -c 'ls /dev/tty{USB*,.wchusbserial*,.usbmodem*,.SLAB_USBtoUART,ACM*} 2>/dev/null') --baud 115200 summary
--- erase.sh
+++ erase.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PATH="$PATH:$(pwd)/xtensa-esp32-elf/bin"
-python2 esp-idf/components/esptool_py/esptool/esptool.py --port $(bash -c 'ls /dev/tty{USB0,.wchusbserial*,.usbmodem*,.SLAB_USBtoUART,ACMx} 2>/dev/null') erase_flash
+python3 esp-idf/components/esptool_py/esptool/esptool.py --port $(bash -c 'ls /dev/tty{USB*,.wchusbserial*,.usbmodem*,.SLAB_USBtoUART,ACM*} 2>/dev/null') erase_flash
