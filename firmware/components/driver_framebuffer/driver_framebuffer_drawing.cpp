9fe516315a451dd64a19d70b9376907a2df6e9ba
RobotMan2412 <julian@scheffers.net>
2020-07-20 12:40:37 +0200

    Triangle drawing functions, but filled rectangles show a seam. Only circle drawing remains unchanged and unaffected by transformations.

--- firmware/components/driver_framebuffer/driver_framebuffer_drawing.cpp
+++ firmware/components/driver_framebuffer/driver_framebuffer_drawing.cpp
@@ -86,9 +86,97 @@ void driver_framebuffer_line(Window* window, int16_t x0, int16_t y0, int16_t x1,
 	}
 }
 
-void driver_framebuffer_triangle(Window* window, int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint32_t color)
+void driver_framebuffer_triangle(Window* window, double x0, double y0, double x1, double y1, double x2, double y2, uint32_t color)
 {
+	//sort the points such that point 0 is the top and point 2 is the bottom
+	//lower number is higher on screen
+	float temp;
+	if (y1 < y0) { //ensure y1 is under y0
+		//swap points 1 and 0
+		temp = y0;
+		y0 = y1;
+		y1 = temp;
+		temp = x0;
+		x0 = x1;
+		x1 = temp;
+	}
+	if (y2 < y1) { //ensure y2 is under y1
+		//swap points 2 and 1
+		temp = y1;
+		y1 = y2;
+		y2 = temp;
+		temp = x1;
+		x1 = x2;
+		x2 = temp;
+	}
+	if (y2 < y0) { //ensure y2 is under y0
+		//swap points 2 and 0
+		temp = y0;
+		y0 = y2;
+		y2 = temp;
+		temp = x0;
+		x0 = x2;
+		x2 = temp;
+	}
+	if (y1 < y0) { //ensure y1 is under y0 once more
+		//swap points 1 and 0
+		temp = y0;
+		y0 = y1;
+		y1 = temp;
+		temp = x0;
+		x0 = x1;
+		x1 = temp;
+	}
+	
+	double yDist = y1 - y0; //between points 0 and 1
+	int nSteps = (int) (yDist + 0.9999); //between points 0 and 1
+	double yStep = yDist / (double) nSteps; //between points 0 and 1
+	double xMiddle = x0 + (x2 - x0) / (y2 - y0) * (y1 - y0);
+	double xStep0 = (xMiddle - x0) / (double) nSteps; //between points 0 and 2
+	double xStep1 = (x1 - x0) / (double) nSteps; //between points 0 and 1
+	//"top" part of the triangle
+	for (int i = 0; i < nSteps; i++) { //go along the rows
+		int y = (int) (y0 + yStep * i + 0.5);
+		int xC0 = (int) (x0 + xStep0 * i + 0.5);
+		int xC1 = (int) (x0 + xStep1 * i + 0.5);
+		int nXSteps = xC0 - xC1;
+		int xStepMult = -1;
+		if (nXSteps < 0) {
+			nXSteps = -nXSteps;
+			xStepMult = 1;
+		}
+		for (int j = 0; j < nXSteps; j++) { //and plot each pixel that falls in it
+			driver_framebuffer_setPixel(window, xC0 + j * xStepMult, y, color);
+		}
+	}
 
+	yDist = y2 - y1; //between points 1 and 2
+	nSteps = (int) (yDist + 0.9999); //between points 1 and 2
+	yStep = yDist / (double) nSteps + 0.01; //between points 1 and 2
+	xStep0 = (x2 - xMiddle) / (double) nSteps; //between points 0 and 2
+	xStep1 = (x2 - x1) / (double) nSteps; //between points 1 and 2
+	//"bottom" part of the triangle
+	for (int i = 0; i < nSteps; i++) { //go along the rows
+		int y = (int) (y1 + yStep * i + 0.5);
+		int xC0 = (int) (xMiddle + xStep0 * i + 0.5);
+		int xC1 = (int) (x1 + xStep1 * i + 0.5);
+		int nXSteps = xC0 - xC1;
+		int xStepMult = -1;
+		if (nXSteps < 0) {
+			nXSteps = -nXSteps;
+			xStepMult = 1;
+		}
+		for (int j = 0; j < nXSteps; j++) { //and plot each pixel that falls in it
+			driver_framebuffer_setPixel(window, xC0 + j * xStepMult, y, color);
+		}
+	}
+}
+
+void driver_framebuffer_quad(Window* window, double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3, uint32_t color)
+{
+	// This is easier to do if represented as two triangles.
+	driver_framebuffer_triangle(window, x0, y0, x1, y1, x3, y3, color);
+	driver_framebuffer_triangle(window, x1, y1, x2, y2, x3, y3, color);
 }
 
 void driver_framebuffer_rect(Window* window, int16_t x, int16_t y, uint16_t w, uint16_t h, bool fill, uint32_t color)
