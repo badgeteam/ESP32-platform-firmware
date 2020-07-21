07769701951f5153e8dadb238dfe1489c35b8231
Tom <mr@tomclement.nl>
2020-07-21 17:52:37 +0200

    Made appconfig api add newly added keys to saved settings. This allows new settings to be added through app updates, and it will update the settings UI for users when the app is ran

--- firmware/python_modules/campzone2020/appconfig.py
+++ firmware/python_modules/campzone2020/appconfig.py
@@ -2,7 +2,18 @@ import valuestore
 
 def get(app_slug, default_config):
     config = valuestore.load(namespace='app', keyname=app_slug)
+    resave = False
+
     if config is None:
         config = default_config
-        valuestore.save(namespace='app', keyname=app_slug, value=default_config)
+        resave = True
+
+    for key in default_config.keys():
+        if key not in config:
+            config[key] = default_config[key]
+            resave = True
+
+    if resave:
+        valuestore.save(namespace='app', keyname=app_slug, value=config)
+
     return config
